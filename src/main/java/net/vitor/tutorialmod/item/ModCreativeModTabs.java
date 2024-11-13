4a566341467bacc4de4bcfba0bc282bf75132166
SeuDemar <seudemar@outlook.com>
2024-11-13 01:47:21 -0300

    Upload texturas, Criando itens, Criando blocos, Atualizando coisas obsoletas.

--- src/main/java/net/vitor/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/vitor/tutorialmod/item/ModCreativeModTabs.java
@@ -4,7 +4,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -20,12 +19,18 @@ public class ModCreativeModTabs {
         () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
         .title(Component.translatable("creative.tab.tutorial_tab"))
         .displayItems((pParameters, pOutput) -> {
+            pOutput.accept(ModItems.METAL_DETECTOR.get());
+            pOutput.accept(ModBlocks.SOUND_BLOCK.get());
+
             pOutput.accept(ModItems.SAPPHIRE.get());
             pOutput.accept(ModItems.RAW_SAPPHIRE.get());
             pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
             pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
 
-            pOutput.accept(Items.DIAMOND);
+            pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+            pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+            pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+            pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
         })
         .build());
     
--- src/main/java/net/vitor/tutorialmod/item/ModItems.java
+++ src/main/java/net/vitor/tutorialmod/item/ModItems.java
@@ -1,12 +1,12 @@
 package net.vitor.tutorialmod.item;
 
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.vitor.tutorialmod.TutorialMod;
+import net.vitor.tutorialmod.item.custom.MetalDetectorItem;
 
 public class ModItems {
 
@@ -19,6 +19,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
         () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+        () -> new MetalDetectorItem(new Item.Properties().durability(100)));
     
 
     public static void register(IEventBus eventBus) {
